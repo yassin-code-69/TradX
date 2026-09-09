19e6110f634614e60adde479f89f7cbab40c1d15
Yassin Bhuyan Alindo <yassincode69@gmail.com>
2026-09-09 23:25:30 +0600

    ..

--- web-admin/next.config.ts
+++ web-admin/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    optimizePackageImports: [
+      "@mantine/core",
+      "@mantine/hooks",
+      "@mantine/dates",
+      "@mantine/notifications",
+      "@mantine/modals",
+      "@tabler/icons-react",
+    ],
+  },
 };
 
 export default nextConfig;
